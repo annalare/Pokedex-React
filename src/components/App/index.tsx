1eec01d42861badbd154b2099519910f9056f1bf
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-08-27 20:51:43 -0300

    app

---
+++ src/components/App/index.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokedex</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { Navbar } from "react-bootstrap"
+
+function Header(){
+return(
+  <Navbar bg="primary"
+)
+}
+
+export default Header
