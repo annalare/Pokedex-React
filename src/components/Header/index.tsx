ac7eb891550aaa3dd182ad597640d7750250c16e
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-08-28 15:33:10 -0300

    components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,24 @@
-import { Navbar } from "react-bootstrap"
+import {
+  Button,
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+} from "react-bootstrap";
 
-function Header(){
-return(
-  <Navbar bg="primary"
-)
+function Header() {
+  return (
+    <Navbar bg="primary" variant="dark" fixed="top">
+      <Container fluid="lg">
+        <Navbar.Brand href="#">Pokedash</Navbar.Brand>
+        <Form className="form-inline">
+          <FormControl type="text" placeholder="Search" />
+          <Button variant="success">Buscar</Button>
+        </Form>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
